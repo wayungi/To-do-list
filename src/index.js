9155ee5f077618bac38a313100c14693d0d85c4d
Francis <wayncis2@gmail.com>
2022-05-12 01:25:39 +0300

    Add todoTask array

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const toDoTasks = [
+  {
+    description:'Watch the chelsea match',
+    completed: false,
+    index: 0
+  },
+  {
+    description:'Take a nap',
+    completed: false,
+    index: 1
+  },
+  {
+    description:'Complete the todo app',
+    completed: false,
+    index: 2
+  }
+]
