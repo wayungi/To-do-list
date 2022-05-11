516235e65b1c0cb738249a0f6b8948c4e035ff62
Francis <wayncis2@gmail.com>
2022-05-12 00:59:20 +0300

    Set development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
 
-  output: {
-    filename: '[name].bundle.js',
-    path:path.resolve(__dirname, 'dist'),
-  },
+  mode: 'development', 
+  
+  entry: './src/index.js',
 
   plugins: [
     new HtmlWebpackPlugin({
@@ -27,6 +25,12 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path:path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 
 
