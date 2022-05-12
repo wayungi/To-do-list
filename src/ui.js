53e279683ab48f2765e656a399ec35bfe04a24e8
Francis <wayncis2@gmail.com>
2022-05-12 12:28:34 +0300

    Add todo list place holder elements

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,7 @@ export default class AppInterface {
 
   static populateToDoList = (tasks) => {
     // imporvements needed = get list from locat storage
-    const list = document.querySelector('.todo');
+    const list = document.querySelector('.todo-list');
     for(let i=0; i<tasks.length; i++){
       const listItem = document.createElement('li');
       listItem.textContent = tasks[i].description;
