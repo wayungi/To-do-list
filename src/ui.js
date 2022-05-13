469d870766c4e71580bba7aef1e490aa319664a2
Francis <wayncis2@gmail.com>
2022-05-13 17:25:59 +0300

    Peer push

--- src/ui.js
+++ src/ui.js
@@ -1,64 +1,68 @@
 export default class AppInterface {
   static populateToDoList = (tasks) => {
-    // sort the array of tasks
     tasks.sort((a, b) => a.index - b.index);
     const outerDiv = document.querySelector('.todo-list');
-    tasks.forEach((task) => {
 
+    tasks.forEach((task) => {
       const activityDiv = document.createElement('div');
       activityDiv.classList.add('main-activity');
-      // create ellipsis
-      const ellipsis = document.createElement('div');
-      ellipsis.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
-      ellipsis.classList.add('ellipsis');
-      // create delete icon
-      const trashCan = document.createElement('div');
-      trashCan.innerHTML = '<i class="fa-solid fa-trash"></i>';
-      trashCan.classList.add('delete-icon', 'hide');
-      // create paragraph
-      const par = document.createElement('p');
-      par.textContent = task.description;
-      par.classList.add('activity-desc');
-      // create a checkbox
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      //create hidden field for element tracking purpose
-      const hiddenField =  document.createElement('input');
-      hiddenField.setAttribute('type', 'hidden');
-      hiddenField.value = task.index;
 
-      activityDiv.appendChild(checkbox);
-      activityDiv.appendChild(par);
-      activityDiv.appendChild(ellipsis);
-      activityDiv.appendChild(trashCan);
-      activityDiv.appendChild(hiddenField);
+      const ellipsis = this.createEllipsisIcon();
+      ellipsis.addEventListener('click', (e) => {
+        ellipsis.classList.add('hide');
+        const deleteBtn = e.target.parentElement.querySelector('.delete-icon');
+        deleteBtn.classList.remove('hide');
+      });
+
+      const trashCan = this.createTrashCanIcon();
+      trashCan.addEventListener('click', (e) => {
+        const mainactivity = document.querySelector('.main-activity');
+        mainactivity.remove();
+        // console.log(e.target.parentElement.parentElement);
+        // console.log('delete activity');
+      });
+
+      const par = this.createToDoTask(task.description);
+      const checkbox = this.createCheckBox();
+      const hiddenField =  this.createHiddenInputField(task.index)
+      activityDiv.append(checkbox, par, ellipsis, trashCan, hiddenField);
       outerDiv.appendChild(activityDiv);
-      //attach eventlisteners to the ellipsis and trashCan
-      this.attachListener(ellipsis,'click', this.enableDeleteBtn);
-      this.attachListener(trashCan, 'click',this.deleteActivity )
     });
+  };
+
+  static createEllipsisIcon = () => {
+    const ellipsis = document.createElement('div');
+    ellipsis.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
+    ellipsis.classList.add('ellipsis');
+    return ellipsis;
+  };
+
+  static createTrashCanIcon = () => {
+    const div = document.createElement('div');
+    div.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    div.classList.add('delete-icon', 'hide');
+    return div;
+  };
+
+  static createToDoTask = (description) => {
+    const par = document.createElement('p');
+    par.textContent = description;
+    par.classList.add('activity-desc');
+    return par;
   }
 
-  //Attach eventListener on creation of element
-  static attachListener = (element, event, func) => {
-    element.addEventListener(event, (e) => func(e));
+  static createCheckBox = () => {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    return checkbox;
   }
 
-  // show delete icon and hide vertical elipsis
-  static enableDeleteBtn = (e) => {
-    // hide the clicked ellipsis
-    e.target.classList.add('hide');
-    //get the delete icon and make it display
-    const parent = e.target.parentElement;
-    const deleteBtn = parent.querySelector('.delete-icon');
-    deleteBtn.classList.remove('hide');
+  static createHiddenInputField = (index) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.value = index;
+    return input;
   }
-
-  static deleteActivity = (e) => {
-    console.log(e.target.parentElement);
-    console.log('delete activity');
-  }
-
 
   // Add a todo task to the interface
   static addTaskToList = (task) => {
