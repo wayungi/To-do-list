f7e677bad770f14d12ee636dce0f3f41c9fd5fda
Francis <wayncis2@gmail.com>
2022-05-14 12:06:10 +0300

    Add method to populate todo list from local storage

--- src/storage.js
+++ src/storage.js
@@ -1,11 +1,6 @@
 export default class Storage {
-  checkStorage = () => {
-    const todoList = localStorage.getItem('todoList');
-    return todoList ? JSON.parse(todoList) : [];
-  }
-
-  // Add a list to localstorage
-  updateStorage = (list) => {
-    localStorage.setItem('todoList', JSON.stringify(list));
+  checkLocalStorage = () => {
+    const tasksArray = localStorage.getItem('tasksArray');
+    return tasksArray ? JSON.parse(tasksArray) : []
   }
 }
