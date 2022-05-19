ed55a789aa2336f2206243ef59e3e6d000f04709
Francis <wayncis2@gmail.com>
2022-05-20 00:06:17 +0300

    Add dummy data

--- src/ui.test.js
+++ src/ui.test.js
@@ -1,2 +1,31 @@
 import AppInterface from "./ui.js";
 
+//add task to the todo list
+describe('Add tasks to the todo list', () => {
+  test('Todo list should have 1 item added', () => {
+
+    document.body.innerHTML = `<div class="todos">`+`</div>`;
+    const todo = document.querySelector('.todos');
+
+    const task1 = { description: 'write first test', 
+                    completed: false, 
+                    index: 1
+                   };
+    const task2 = { description: 'Debug first test', 
+                    completed: false, 
+                    index: 2
+    };
+    const task3 = { description: 'Finish up the test', 
+                    completed: true, 
+                    index: 3
+   };
+
+   
+
+
+
+  });
+});
+
+
+//remove task from todo list
